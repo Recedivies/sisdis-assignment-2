fdbf46dcf53facf4eff38c43294c8b558da23ef2
recedivies <ahmadhiprananta@gmail.com>
2024-02-22 13:19:51 +0700

    Progress 90%

--- main.py
+++ main.py
@@ -134,15 +134,94 @@ def listening_procedure(port, timeout, stop_event):
     # Create a socket to listen to the nodes
     # Arguments: Main Node port, timeout duration, threading stop_event
     # See example/main.txt for example output
-    """"""
+
+    logger.info("Listening for incoming messages")
+
+    udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_client_socket.bind(("127.0.0.1", port))
+    udp_client_socket.settimeout(timeout)
+
+    while not stop_event.is_set():
+        try:
+            input_value_byte, address = udp_client_socket.recvfrom(1024)
+            message_raw = input_value_byte.decode("UTF-8")
+
+            logger.debug(f"message_raw: {message_raw}")
+
+            splitted_message = message_raw.split("#")
+            sender_node = splitted_message[0]
+            status_dictionary_node = splitted_message[1]
+
+            dict_status_dictionary = literal_eval(status_dictionary_node)
+
+            logger.info(f"Receive message from {sender_node}...")
+
+            logger.info(f"Incoming message:\n{pformat(dict_status_dictionary)}")
+
+            logger.debug(f"address: {address}")
+            logger.debug("Process the message...")
+
+            for node_id in range(1, 5):
+                node = f"node-{node_id}"
+
+                logger.debug(f"key: {node}")
+
+                current_logical_time = main_status_dictionary[node][0]
+                current_status = main_status_dictionary[node][1]
+
+                input_logical_time = dict_status_dictionary[node][0]
+                input_status = dict_status_dictionary[node][1]
+
+                logger.debug(f"input_node_list {node}: {dict_status_dictionary[node]}")
+                logger.debug(
+                    f"current_node_list {node}: {main_status_dictionary[node]}"
+                )
+
+                if (
+                    current_logical_time >= input_logical_time
+                    and current_status == input_status
+                ):
+                    logger.debug("Skip this loop...")
+                    continue
+
+                logger.debug(
+                    f"Update logical time {node}: {current_logical_time} -> {input_logical_time}"
+                )
+                main_status_dictionary[node][0] = input_logical_time
+                main_status_dictionary[node][1] = input_status
+
+                logger.debug(f"Check if {node} has died...")
+
+                if input_status == True:
+                    logger.debug(f"{node} has not died...")
+                else:
+                    logger.debug(f"{node} has died...")
+
+            logger.info(
+                f"main_status_dictionary after incoming message:\n{pformat(main_status_dictionary)}"
+            )
+
+        except socket.timeout:
+            pass
+        except Exception as e:
+            logger.error(f"Error occurred: {e}")
 
 
-def check_node_status(port):
+def check_node_status(port, main_port):
     # TODO
     # Create a socket to get node status dictionary
     # Arguments: Node port
     # See example/main.txt for example output
-    """"""
+
+    tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_client_socket.bind(("127.0.0.1", main_port))
+
+    message = "check_status"
+    tcp_client_socket.connect(("127.0.0.1", port))
+    tcp_client_socket.sendall(message.encode("UTF-8"))
+    status_dictionary_node = tcp_client_socket.recv(1024).decode("UTF-8")
+
+    return literal_eval(status_dictionary_node)
 
 
 def shutdown_nodes(kill_duration):
@@ -180,17 +259,62 @@ def interactive_mode(starting_port, port_used, args):
                 # Check the Status Dictionary of the node with the given node_id
                 # Should print Status Dictionary like status command above
 
+                port_node = starting_port + node_id - 1
+
+                logger.info(f"Checking node-{node_id} with port {port_node} status...")
+
+                status_dictionary_node = check_node_status(
+                    port=port_node, main_port=starting_port - 1
+                )
+
+                logger.info(
+                    f"Node node-{node_id} status dictionary:\n{pformat(status_dictionary_node)}"
+                )
+
             elif command[0] == "start" and len(command) == 2:
                 node_id = int(command[1])
                 # TODO
                 # Start the node with the given node_id
                 # Configuration should match the one in setup_nodes
 
+                node_name = f"node-{node_id}"
+                if node_name not in node_dictionary:
+                    process = NodeProcess(
+                        target=node.main,
+                        name=node_name,
+                        args=(
+                            float(args.heartbeat),
+                            int(args.neighbors),
+                            float(args.fault_duration),
+                            starting_port + node_id - 1,
+                            node_id,
+                            port_used,
+                            starting_port - 1,
+                        ),
+                    )
+                    process.start()
+                    node_dictionary[node_name] = process
+
+                    logger.info(f"Node {node_id} started.")
+                    logger.info(f"Node Dictionary:\n{pformat(node_dictionary)}")
+                else:
+                    logger.info(f"Node {node_id} is already running.")
+
             elif command[0] == "kill" and len(command) == 2:
                 node_id = int(command[1])
                 # TODO
                 # Kill the node with the given node_id
                 # All nodes should mark this node as fault in their Status Dictionary automatically
+
+                node_name = f"node-{node_id}"
+                if node_name in node_dictionary:
+                    process = node_dictionary.pop(node_name)
+                    process.kill()
+
+                    logger.info(f"Node {node_id} killed.")
+                    logger.info(f"Node Dictionary:\n{pformat(node_dictionary)}")
+                else:
+                    logger.info(f"Node {node_id} is not running.")
 
             elif command[0] == "shutdown":
                 print("Shutting down all nodes")
--- node.py
+++ node.py
@@ -7,14 +7,20 @@ import socket
 from ast import literal_eval
 from pprint import pformat
 
+timer_dict = {}
+
+
 def start_fault_timer(key):
     status_dictionary[key][1] = False
     logger.info(f"This node become a fault: {key}")
     logger.info(f"Node fault status_dictionary:\n{pformat(status_dictionary)}")
 
+
 def send_message(node_id, port):
     logger.debug("Create the client socket")
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Note that this socket is UDP
+    client_socket = socket.socket(
+        socket.AF_INET, socket.SOCK_DGRAM
+    )  # Note that this socket is UDP
     logger.debug("Encode the message")
     message = f"node-{node_id}#{status_dictionary}"
     logger.debug(f"message: {message}")
@@ -23,13 +29,41 @@ def send_message(node_id, port):
     logger.debug("Send the message")
     client_socket.sendto(message, addr)
 
-def sending_procedure(heartbeat_duration, node_id, neighbors_port, node_ports, main_port):
-    logger.info("Sending Procedure Thread Created")
+
+def sending_procedure(
+    heartbeat_duration,
+    node_id,
+    neighbors_port,
+    node_ports,
+    main_port,
+    num_of_neighbors_to_choose,
+):
     # TODO
     # Create a socket to send the heartbeat to the neighbors and main node
     # Arguments: Heartbeat duration, this Node ID, Neighbors port, Node ports, Main Node port
     # Note: use send_message function to send the heartbeat
-    ""
+
+    while True:
+        # Increase heartbeat node-i
+        status_dictionary[f"node-{node_id}"][0] += 1
+        logger.info(f"Increase heartbeat node-{node_id}:\n{pformat(status_dictionary)}")
+
+        logger.info("Determining which node to send...")
+
+        random_neighbors = random.sample(neighbors_port, num_of_neighbors_to_choose)
+        node_names = [f"node-{node_ports[node]}" for node in random_neighbors]
+        log_message = "Send messages to main node, " + ", ".join(node_names)
+
+        logger.info(log_message)
+
+        send_message(node_id=node_id, port=main_port)
+        # print(random_neighbors, "\n")
+        for neighbor_port in random_neighbors:
+            message = f"Heartbeat from node {node_id}"
+            send_message(node_id=node_id, port=neighbor_port)
+
+        time.sleep(heartbeat_duration)
+
 
 def fault_timer_procedure(node_id, fault_duration):
     for key in status_dictionary.keys():
@@ -38,38 +72,154 @@ def fault_timer_procedure(node_id, fault_duration):
             continue
         thread = threading.Timer(fault_duration, start_fault_timer, (key,))
         thread.start()
-        
+        timer_dict[key] = thread
+
+
 def tcp_listening_procedure(port, node_id):
-    logger.info("Initiating TCP socket")
     # TODO
     # Create a TCP socket to listen to the main node
     # Arguments: Main Node port, Node ID
-    ""
-                    
+
+    logger.info("Initiating TCP socket")
+
+    tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_client_socket.bind(("127.0.0.1", port))
+    tcp_client_socket.listen(1)
+
+    logger.info("Listening for incoming TCP connections...")
+
+    while True:
+        connection, address = tcp_client_socket.accept()
+        logger.info(f"Connected by {address}")
+
+        input_value = connection.recv(1024).decode("UTF-8")
+        logger.info("Received status request, sending status data...")
+
+        message = f"{status_dictionary}"
+        message = message.encode("UTF-8")
+
+        connection.send(message)
+
+        connection.close()
+
+
 def listening_procedure(port, node_id, fault_duration):
     # TODO
     # Create a UDP socket to listen to all other node
     # Arguments: This Node's port, Node ID, fault duration
-    ""
+
+    logger.info("Start the timer for fault duration...")
+
+    fault_timer_procedure(node_id=node_id, fault_duration=fault_duration)
+
+    logger.info("Initiating socket...")
+
+    udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_client_socket.bind(("127.0.0.1", port))
+
+    logger.info("Listen for incoming messages...")
+
+    while True:
+        input_value_byte, address = udp_client_socket.recvfrom(1024)
+        message_raw = input_value_byte.decode("UTF-8")
+
+        logger.info(f"message_raw: {message_raw}")
+
+        splitted_message = message_raw.split("#")
+        sender_node = splitted_message[0]
+        status_dictionary_node = splitted_message[1]
+
+        # TODO !!!
+        # Handle that receive from node-i, it means we need to restart timer of fault_timer_procedure
+        # But how (?)
+
+        if sender_node in timer_dict:
+            timer_dict[sender_node].cancel()
+            thread = threading.Timer(fault_duration, start_fault_timer, (key,))
+            thread.start()
+            timer_dict[sender_node] = thread
+
+        dict_status_dictionary = literal_eval(status_dictionary_node)
+
+        logger.info(f"Receive message from node-{sender_node}...")
+
+        logger.info(f"Incoming message:\n{pformat(dict_status_dictionary)}")
+
+        logger.debug(f"address: {address}")
+        logger.debug("Process the message...")
+
+        for node_id in range(1, 5):
+            node = f"node-{node_id}"
+
+            current_logical_time = status_dictionary[node][0]
+            current_status = status_dictionary[node][1]
+
+            input_logical_time = dict_status_dictionary[node][0]
+            input_status = dict_status_dictionary[node][1]
+
+            logger.debug(f"input_node_list {node}: {dict_status_dictionary[node]}")
+            logger.debug(f"current_node_list {node}: {status_dictionary[node]}")
+
+            if (
+                current_logical_time >= input_logical_time
+                and current_status == input_status
+            ):
+                logger.debug("Skip this loop...")
+                continue
+
+            logger.debug(
+                f"Update logical time {node}: {current_logical_time} -> {input_logical_time}"
+            )
+            status_dictionary[node][0] = input_logical_time
+            status_dictionary[node][1] = input_status
+
+            logger.debug(f"Check if {node} has died...")
+
+            if input_status == True:
+                logger.debug(f"{node} has not died...")
+            else:
+                logger.debug(f"{node} has died...")
+
+        logger.info(
+            f"status_dictionary after incoming message:\n{pformat(status_dictionary)}"
+        )
+
 
 def handle_exception(exc_type, exc_value, exc_traceback):
-    logger.error(f"Uncaught exception handler", exc_info=(exc_type, exc_value, exc_traceback))
+    logger.error(
+        f"Uncaught exception handler", exc_info=(exc_type, exc_value, exc_traceback)
+    )
+
 
 def thread_exception_handler(args):
-    logger.error(f"Uncaught exception", exc_info=(args.exc_type, args.exc_value, args.exc_traceback))
+    logger.error(
+        f"Uncaught exception",
+        exc_info=(args.exc_type, args.exc_value, args.exc_traceback),
+    )
+
 
 def reload_logging_windows(filename):
     log = logging.getLogger()
     for handler in log.handlers:
         log.removeHandler(handler)
-    logging.basicConfig(format='%(asctime)-4s %(levelname)-6s %(threadName)s:%(lineno)-3d %(message)s',
-                        datefmt='%H:%M:%S',
-                        filename=filename,
-                        filemode='w',
-                        level=logging.DEBUG)
+    logging.basicConfig(
+        format="%(asctime)-4s %(levelname)-6s %(threadName)s:%(lineno)-3d %(message)s",
+        datefmt="%H:%M:%S",
+        filename=filename,
+        filemode="w",
+        level=logging.DEBUG,
+    )
 
-def main(heartbeat_duration=1, num_of_neighbors_to_choose=1,
-         fault_duration=1, port=1000, node_id=1, neighbors_ports=[1000,1001], main_port=999):
+
+def main(
+    heartbeat_duration=1,
+    num_of_neighbors_to_choose=1,
+    fault_duration=1,
+    port=1000,
+    node_id=1,
+    neighbors_ports=[1000, 1001],
+    main_port=999,
+):
     reload_logging_windows(f"logs/node{node_id}.txt")
     global logger
     logger = logging.getLogger(__name__)
@@ -87,7 +237,7 @@ def main(heartbeat_duration=1, num_of_neighbors_to_choose=1,
         node_ports = {}
         for i in range(len(neighbors_ports)):
             status_dictionary[f"node-{i + 1}"] = [0, True]
-            node_ports[neighbors_ports[i]] = i+1
+            node_ports[neighbors_ports[i]] = i + 1
         neighbors_ports.remove(port)
         logger.debug(f"final neighbors_ports: {neighbors_ports}")
         logger.info(f"status_dictionary:\n{pformat(status_dictionary)}")
@@ -100,19 +250,30 @@ def main(heartbeat_duration=1, num_of_neighbors_to_choose=1,
 
         logger.info("Executing the listening procedure")
         threading.excepthook = thread_exception_handler
-        thread = threading.Thread(target=listening_procedure, args=(port, node_id, fault_duration))
+        thread = threading.Thread(
+            target=listening_procedure, args=(port, node_id, fault_duration)
+        )
         thread.name = "listening_thread"
         thread.start()
-        
+
         logger.info("Executing the sending procedure")
-        thread = threading.Thread(target=sending_procedure,
-                         args=(heartbeat_duration,
-                               node_id, neighbors_ports, node_ports, main_port))
+        thread = threading.Thread(
+            target=sending_procedure,
+            args=(
+                heartbeat_duration,
+                node_id,
+                neighbors_ports,
+                node_ports,
+                main_port,
+                num_of_neighbors_to_choose,
+            ),
+        )
         thread.name = "sending_thread"
         thread.start()
     except Exception as e:
         logger.exception("Caught Error")
         raise
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
